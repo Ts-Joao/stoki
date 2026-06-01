12ec8233c2d0015c07ce50006219d59a1de2bb8f
ts-joao <cttjoaovts@gmail.com>
2026-06-01 17:43:49 -0300

    feat: adiciona endpoints e funcoes ao sistema de autenticacao

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Post, Body } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.authenticate(loginDto);
+  }
+
+  @Post('logout')
+  async logout(@Body() body: { userId: string }) {
+    return this.authService.logout(body.userId);
+  }
+
+  @Post('refresh')
+  async refresh(@Body() body: { userId: string, refreshToken: string }) {
+    return this.authService.refreshTokens(body.userId, body.refreshToken);
+  }
+}
